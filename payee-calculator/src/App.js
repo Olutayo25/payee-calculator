c30982c876018efd910513f114f8b811c9b92d9e
Olutayo25 <olutayoofficial@gmail.com>
2025-03-19 06:24:30 +0100

    Configured GitHub Pages deployment

---
+++ payee-calculator/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PAYEECalculator from "./PAYEECalculator";
+
+function App() {
+  return (
+    <div className="App">
+      <PAYEECalculator />
+    </div>
+  );
+}
+
+export default App;
